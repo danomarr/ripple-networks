5485d841a85147b25bd104d1a4fcaa1ed5d2a66c
Dano Marr <danomarr@gmail.com>
2015-11-14 14:14:15 +0100

    Added full site to versioning

---
+++ contact.php
@@ -0,0 +1,98 @@
+<?php include 'header.php'; ?> 
+
+        <div class="main-container">
+            <section class="page-title page-title-4 image-bg overlay">
+                <div class="background-image-holder">
+                    <img alt="Background Image" class="background-image" src="img/home-2-1.jpg" />
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3 class="uppercase mb0">About Us</h3>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+                <ol class="breadcrumb breadcrumb-2">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Pages</a>
+                    </li>
+                    <li class="active">About Us</li>
+                </ol>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h5>Press</h5>
+                            <p>
+                                For press related inquiries, contact <a href="mailto:press@ripplenetworks.com">press@ripplenetworks.com</a>.
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <h5 class="">Ripple Networks USA</h5>
+                            <p>
+                                <strong>Tenerife, Spain</strong><br>
+                                Plaza del Charco 7<br>
+                                38400 Puerto de la Cruz<br>
+                                Santa Cruz de Tenerife
+                            </p>
+                            <p type="tel:18475555555">+1-847-555-5555</p>   
+                        </div>
+                        <div class="col-sm-3">
+                            <h5 class="">Ripple Networks Europe</h5>
+                            <p>
+                                <strong>Tenerife, Spain</strong><br>
+                                Plaza del Charco 7<br>
+                                38400 Puerto de la Cruz<br>
+                                Santa Cruz de Tenerife
+                            </p>
+                            <p type="tel:18475555555">+1-847-555-5555</pf>                            
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+          <footer class="footer-2 bg-dark text-center-xs">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <a href="#"><img class="image-xxs fade-half" alt="Pic" src="img/logo-light.png"></a>
+                        </div>
+                    
+                        <div class="col-sm-4 text-center">
+                            <span class="fade-half">
+                                © Copyright 2015 Ripple Networks - All Rights Reserved
+                            </span>
+                        </div>
+                    
+                        <div class="col-sm-4 text-right text-center-xs">
+                            <ul class="list-inline social-list">
+                                <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
+                                <li><a href="#"><i class="ti-facebook"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/flickr.js"></script>
+        <script src="js/flexslider.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/masonry.min.js"></script>
+        <script src="js/twitterfetcher.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/ytplayer.min.js"></script>
+        <script src="js/countdown.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ memberships.php
@@ -0,0 +1,98 @@
+<?php include 'header.php'; ?>
+        <div class="main-container">
+            <section class="page-title page-title-4 image-bg overlay">
+                <div class="background-image-holder">
+                    <img alt="Background Image" class="background-image" src="img/home-2-1.jpg" />
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3 class="uppercase mb0">Memberships</h3>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+                <ol class="breadcrumb breadcrumb-2">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Pages</a>
+                    </li>
+                    <li class="active">About Us</li>
+                </ol>
+            </section>
+            <section>
+                <div class="container">
+                        
+                        <div class="col-sm-12 col-md-8 col-md-offset-2 mb32">
+                            <h4 class="">
+                            Ripple Networks is a member of the following sites.
+                            </h4>
+                            <hr>
+                            <div class="row">
+                                <div class='col-md-4 col-sm-6 boxed'>
+                                    <a href="https://www.crunchbase.com/organization/ripple-news#/entity" target="_blank">Crunchbase</a>
+                                </div>
+                                <div class='col-md-4 col-sm-6 boxed'>
+                                    <a href="http://www.theiabm.org/membership/iabm-members.html?sort=R" target="_blank" >IABM</a>
+                                </div>
+                                <div class='col-md-4 col-sm-6 boxed'>
+                                    <a href="http://www.w3.org/Consortium/Member/List" target="_blank" >W3.org</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class='col-md-4 col-sm-6 boxed'>
+                                    <a href="https://www.linkedin.com/company/3284524?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A3284524%2Cidx%3A2-1-4%2CtarId%3A1441125513755%2Ctas%3Aripple%20networks" target="_blank" >LinkedIn</a>
+                                </div>
+                                <div class='col-md-4 col-sm-6 boxed'>
+                                    <a href="http://dashif.org/members/" target="_blank" >DASH</a>
+                                </div>
+                                <div class='col-md-4 col-sm-6 boxed'>
+                                    <a href="http://www.imtc.org/membership/member-list/" target="_blank" >IMTC</a>
+                                </div>
+                            </div>
+                        </div>
+
+                </div>
+                <!--end of container-->
+            </section>
+          <footer class="footer-2 bg-dark text-center-xs">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <a href="#"><img class="image-xxs fade-half" alt="Pic" src="img/logo-light.png"></a>
+                        </div>
+                    
+                        <div class="col-sm-4 text-center">
+                            <span class="fade-half">
+                                © Copyright 2015 Ripple Networks - All Rights Reserved
+                            </span>
+                        </div>
+                    
+                        <div class="col-sm-4 text-right text-center-xs">
+                            <ul class="list-inline social-list">
+                                <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
+                                <li><a href="#"><i class="ti-facebook"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/flickr.js"></script>
+        <script src="js/flexslider.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/masonry.min.js"></script>
+        <script src="js/twitterfetcher.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/ytplayer.min.js"></script>
+        <script src="js/countdown.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
