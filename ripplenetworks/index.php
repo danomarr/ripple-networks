f9a77d86262e6bbc2752c49b290b7f0fd885d0c2
Dano Marr <danomarr@gmail.com>
2015-11-18 22:30:46 +0100

    added new fortrabbit app

---
+++ ripplenetworks/index.php
@@ -0,0 +1,149 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include ($path. "/includes/header.html");
+   include ($path. "/includes/navigation.html");
+?>
+	
+		<div class="main-container">
+					
+			<section class="fullscreen cover image-slider slider-all-controls controls-inside image-bg overlay">
+		        <ul class="slides">
+                	<li class="overlay image-bg bg-light">
+						<div class="background-image-holder">
+				            <img alt="image" class="background-image" src="img/home-2-6.jpg">
+				        </div>
+				        <div class="container v-align-transform">
+				            <div class="row">
+				                <div class="col-sm-12 text-center">
+				                    <h1 class="mb16">We are re-architecting the internet <br>to fulfill the promise of OTT video.<br></h1>
+				                    <h6 class="uppercase mb32">RIPPLE NETWORKS DELIVERS EXPONENTIAL VIDEO FOR THE DIGITAL AGE.</h6>
+				                    <div class="modal-container">
+				                        <div class="play-button btn-modal inline large"></div>
+				                        <div class="foundry_modal no-bg">
+				                            <iframe data-provider="vimeo" data-video-id="25737856" data-autoplay="1"></iframe>
+				                        </div>
+				                    </div>
+				                    
+				                </div>
+				            </div>
+				            
+				        </div>
+				    </li>
+                	<li class="overlay image-bg bg-light">
+						<div class="background-image-holder">
+				            <img alt="image" class="background-image" src="img/home-2-7.jpg">
+				        </div>
+				        <div class="container v-align-transform">
+				            <div class="row">
+				                <div class="col-sm-12 text-center">
+				                    <h1 class="mb16">We didn't invent the internet, <br>we just made it better.</h1>
+				                    <h6 class="uppercase mb32">RIPPLE NETWORKS DELIVERS EXPONENTIAL VIDEO FOR THE DIGITAL AGE.</h6>
+				                    <div class="modal-container">
+				                        <div class="play-button btn-modal inline large"></div>
+				                        <div class="foundry_modal no-bg">
+				                            <iframe data-provider="vimeo" data-video-id="25737856" data-autoplay="1"></iframe>
+				                        </div>
+				                    </div>
+				                    
+				                </div>
+				            </div>
+				            
+				        </div>
+				    </li>
+                	<li class="overlay image-bg bg-light">
+						<div class="background-image-holder">
+				            <img alt="image" class="background-image" src="img/home-2-8.jpg">
+				        </div>
+				        <div class="container v-align-transform">
+				            <div class="row">
+                                <div class="col-md-6 col-sm-8">
+                                    <h1 class="mb40 mb-xs-16">We're going to Streaming Media West.</h1>
+                                    <p class="lead mb40">
+                                        We are on the move, making waves and shaking the 
+                                        <br />foundation of an entire industry.
+                                    </p>
+                                    <a class="btn btn-lg" href="#">About Us</a>
+                                </div>
+                            </div>
+				            
+				        </div>
+				    </li>
+				</ul>
+		        
+		    </section><section class="cta-thin">
+		    	<div class="row">
+	                <div class="col-sm-12 text-center">
+	                    <p class="lead mb0 inline-block p32 p0-xs">Use the video transport of the future to improve your ROI.</p>
+	                    <a class="btn mb0 mt-xs-24" href="#">DEPLOY RIPPLE</a>
+	                </div>
+	            </div>
+		    </section>
+		    	<hr>
+		    <section>
+		        <div class="container">
+		            <!-- <div class="row">
+		                <div class="col-md-12 mb80 text-center">
+		                    <h2 class="">Use the video transport of the future to improve your ROI.<br></h2>
+		                </div>
+		            </div> -->
+		            
+		            <div class="row mb32 mb-xs-0">
+		                <div class="col-md-10 col-sm-8">
+		                    <h3 class="">
+		                        Are Video Delivery Problems eroding your viewers with: <br>Startup times, Buffering, Latency, Peak Loads, Costs?
+		                    </h3>
+		                </div>
+		            </div>
+            		<div class="row v-align-children mb80">
+		               <div class="col-md-6 col-sm-6 mb-xs-24">
+		                    <h4>Ripple Networks can solve it for you.</h4>
+		                    <hr>
+		                    <p class="lead">When Viewers churn away before the video starts -- ROI is impacted.  Ripple solves slow startup times by pushing files so that the video player does not need to request them. Viewers see buffering from internet network congestions, Ripple monitors Internet peering points, predicts congestions, and proactively re-routes the video packages to reach their destination in time. <br><br>Latency is solved by using WebRTC (Web Real Time Communications) to transmit the data. During peak times many viewers start viewing in short time frames that put loads on servers and outgoing routers congesting them. Ripple utilizes the browser to browser sharing built into WebRTC to spread the load efficiency driving down peaks, significantly reducing costs, and reaching more of the audience.<br></p>		                    
+
+
+		                </div>
+		                <div class="col-md-6 col-md-offset-1 col-sm-6 col-sm-offset-1 text-center">
+		                    <img class="cast-shadow" alt="Screenshot" src="img/home-2-4.jpg">
+		                </div>
+		            </div> 
+		            
+		            <div class="row">
+		                <div class="col-md-10 mb64">
+		                    <h2 class="">Since Ripple got you this far, why not go all the way?<br></h2>
+		                </div>
+		            </div>
+		            
+		            <div class="row mb64">
+            			<div class="col-sm-12 col-md-8 col-md-offset-2">
+            				<h4 class="">Let’s improve Reach, Management, Analytics and Support.</h4>
+            				<hr>
+            				<p class="lead mb64">When using Adaptive Streaming, the further from the Point of Origin the viewer is located, the lower the resolution. Viewers expecting high resolution receive a degraded experience. Ripple delivers the highest quality to the furthest reaches of the internet by using our private routing tables and creating a Virtual Data Trunk.
+		                    </p>		                    
+
+		                    <h4 class="">Ripple gives customers the ultimate remote control dashboard.</h4>
+            				<hr>
+            				<p class="lead mb64">Login to the Ripple Playout Control Center to easily add, modify, schedule, start and stop broadcasts and set-up your channels. In the Analytics Control Center monitor and analyze broadcasts, the analytics data can also collected, store and deliver through an API directly into your in-house management system. The Ripple Call in Support tool gives the call responder real time information about any and all problems with the broadcast down to an individual level, and the responder can thereby give the caller the relevant information as well as make an important changes.
+		                    </p>		                    
+            			</div> 
+            		</div>
+
+		        </div>
+		        
+		    </section><a id="cta-with-one-liner" class="in-page-link"></a>
+			
+			<section class="bg-primary">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-12 text-center">
+		                    <h3 class="mb0 inline-block p32 p0-xs">Join us at the forefront of internet video delivery.</h3>
+		                    <a class="btn btn-lg btn-white mb8 mt-xs-24" href="#">DEPLOY RIPPLE</a>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section>
+
+<?php include ($path. "/includes/footer.html"); ?>
+ 
+				
